b3a86021a4a63844bde99252f54ff6465f27e55f
doctoramalvada <mydemoni@hotmail.com>
2019-05-07 09:38:46 -0500

    deploy arreglado, enlace hecho

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import firebase from 'firebase';
 import './index.css';
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyA6EX_Y2l7MxOaJmAfPx5HUAYUIpX6WtpE",
+//     authDomain: "ely-burguer-queen.firebaseapp.com",
+//     databaseURL: "https://ely-burguer-queen.firebaseio.com",
+//     projectId: "ely-burguer-queen",
+//     storageBucket: "ely-burguer-queen.appspot.com",
+//     messagingSenderId: "324480551011",
+//     appId: "1:324480551011:web:4aebb526702756f0"
+// })
+
 class Square extends React.Component {
     render() {
       return (
