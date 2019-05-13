7a2682d4352df03598bb1107346f7f21880ff871
doctoramalvada <mydemoni@hotmail.com>
2019-05-13 14:13:41 -0500

    instento jalar data de firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
 import  './App.js';
-import firebase from 'firebase'
+// import firebase from 'firebase'
+// import 'firebase/database';
 
 
+// const config = {
+//     apiKey: "AIzaSyA6EX_Y2l7MxOaJmAfPx5HUAYUIpX6WtpE",
+//     authDomain: "ely-burguer-queen.firebaseapp.com",
+//     databaseURL: "https://ely-burguer-queen.firebaseio.com",
+//     projectId: "ely-burguer-queen",
+//     storageBucket: "ely-burguer-queen.appspot.com",
+//     messagingSenderId: "324480551011",
+//     appId: "1:324480551011:web:4aebb526702756f0"
+//   };
+
+
+// firebase.initializeApp(config)
+
+// class App extends React.Component{
+//     constructor(){
+//         super()
+//         this.state ={
+//             name:'Ely'
+//         }
+//     }
+
+//     componentWillMount(){
+//         const nameRef = firebase.database().ref().child('objet').child('name')
+//         nameRef.on('value',(snapshot)=>{
+//             this.setState({
+//                 name:snapshot.val()
+//             })
+//         })
+//     }
+//     render() {
+//         return<h1>Hola {this.state.name}</h1>
+//     }
+// }
+// ReactDOM.render(<App/>, document.getElementById('root'))
 
