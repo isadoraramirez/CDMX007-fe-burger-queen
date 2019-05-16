117bedb46ad66c1206c5c2987ffca5fa86d3ad39
doctoramalvada <mydemoni@hotmail.com>
2019-05-15 20:19:16 -0500

    intento subir json

--- src/components/breakfast.js
+++ src/components/breakfast.js
@@ -23,4 +23,14 @@ const Breakfast = (props) => {
   
     
   };
+  fetch('./datamenu.json').then(response => {
+    console.log(response);
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+  }).catch(err => {
+    // Do something for an error here
+    console.log("Error Reading data " + err);
+  });
 export default Breakfast;
