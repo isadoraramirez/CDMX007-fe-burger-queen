fe1db36f1cab7b5ddb4371ad12a40c0ff0b2f5da
doctoramalvada <mydemoni@hotmail.com>
2019-05-15 19:54:14 -0500

    regreso a commit, todo se creasheo >_<

--- src/components/breakfast.js
+++ src/components/breakfast.js
@@ -1,10 +1,26 @@
-fetch('./data.json').then(response => {
-  console.log(response);
-  return response.json();
-}).then(data => {
-  // Work with JSON data here
-  console.log(data);
-}).catch(err => {
-  // Do something for an error here
-  console.log("Error Reading data " + err);
-});
+import React from 'react';
+import{Datamenu} from "../data/datamenu"
+//import { Link } from 'react-router-dom';
+// import breakfast from './breakfast';
+// import { Route} from 'react-router-dom';
+
+const Breakfast = (props) => {
+    let menu = Datamenu.map((dish)=>{
+        return (
+            <div className="menu-Container">
+                <button>{dish.name} <br></br> $ 
+                {dish.price}</button>
+            </div>
+        );
+    });
+  return (
+      <div className="main-content">
+          <div className= "container">
+              {menu}
+          </div>
+      </div>
+  );
+  
+    
+  };
+export default Breakfast;
