59b0f843175fbb61fef27e2d2100393132070341
doctoramalvada <mydemoni@hotmail.com>
2019-05-24 12:39:53 -0500

    arreglo service worker

--- src/index.js
+++ src/index.js
@@ -1,44 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import  './App.js';
+import App from './App.js';
+import * as serviceWorker from './registerServiceWorker';
 
-// import firebase from 'firebase'
-// import 'firebase/database';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-// const config = {
-//     apiKey: "AIzaSyA6EX_Y2l7MxOaJmAfPx5HUAYUIpX6WtpE",
-//     authDomain: "ely-burguer-queen.firebaseapp.com",
-//     databaseURL: "https://ely-burguer-queen.firebaseio.com",
-//     projectId: "ely-burguer-queen",
-//     storageBucket: "ely-burguer-queen.appspot.com",
-//     messagingSenderId: "324480551011",
-//     appId: "1:324480551011:web:4aebb526702756f0"
-//   };
-
-
-// firebase.initializeApp(config)
-
-// class App extends React.Component{
-//     constructor(){
-//         super()
-//         this.state ={
-//             name:'Ely'
-//         }
-//     }
-
-//     componentWillMount(){
-//         const nameRef = firebase.database().ref().child('objet').child('name')
-//         nameRef.on('value',(snapshot)=>{
-//             this.setState({
-//                 name:snapshot.val()
-//             })
-//         })
-//     }
-//     render() {
-//         return<h1>Hola {this.state.name}</h1>
-//     }
-// }
-// ReactDOM.render(<App/>, document.getElementById('root'))
+serviceWorker.unregister();
 
