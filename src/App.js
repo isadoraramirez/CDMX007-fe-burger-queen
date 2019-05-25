0ca455c29a11960def413618aacadd0b5a1d7628
doctoramalvada <mydemoni@hotmail.com>
2019-05-25 15:24:03 -0500

    fusiono ramas

--- src/App.js
+++ src/App.js
@@ -8,19 +8,32 @@ import Home from './components/home';
 import Breakfast from './components/breakfast';
 import Meal from './components/meal';
 
+
+// import { AppContextProvider, AppContext } from "./appContext";
+import Item from "./components/item"
+import Button from "./components/button";
+
+
 class App extends Component {
     
     render() {
         return (
+            
             <BrowserRouter>
                 <div className="App">
                     <Navbar />
                     <Route exact path="/" render={() => <Home title="Bienvenido" />} />
                     <Route path="/breakfast" render={() => <Breakfast title="Desayuno" />} />
                     <Route path="/meal" render={() => <Meal title="Comida" />} />
+                
+                
+                    <Route path="/complements" render={() => <Item />} />
+            <Route path="/complements" render={() => <Button />} />
+                
                 </div>
             </BrowserRouter>
         );
+           
   }
 }
 
--- src/components/breakfast.js
+++ src/components/breakfast.js
@@ -1,25 +1,109 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Item from './item';
+import Button from './button';
+import _ from 'lodash';
 
-import axios from 'axios';
 
-export default class List extends React.Component {
-  state = {
-    items: []
+
+
+ class Breakfast extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+      // ,
+      // order: []
+    };
+    // this.btnSelect = this.btnSelect.bind(this);
+    // this.btnDelete = this.btnDelete.bind(this);
   }
 
   componentDidMount() {
-    axios.get(`https://ely-burguer-queen.firebaseio.com/menu.json`)
-      .then(res => {
-        const items = res.data;
-        this.setState({ items });
-      })
+    fetch("https://ely-burguer-queen.firebaseio.com/menu.json").then(res =>
+    
+   
+    res.json().then(json => {
+        console.log(json);      
+        this.setState({
+          data: json
+          
+        });  
+    })
+    );
   }
+
+
+  _remove(position){
+    let { data } = this.state;
+
+    let newData = [
+      ...data.slice(0, position),
+      ...data.slice(position + 1),
+    ]
+
+    this.setState({ data : newData });
+
+  }
+
+
+
+  _add(position){
+    let { data } = this.state;
+
+    let newData = [
+      ...data.slice(0, position),
+      ...data.slice(position + 1),
+    ]
+
+    this.setState({ data : newData });
+
+  }
+
+
+
+
+  _getTotal(){
+    return _.sumBy(this.state.data, function(o) { return o.price; });;
+  }
+
+
 
   render() {
     return (
-      <ul>
-       { this.state.items.map(obj => <li> <button>{obj.name} $ {obj.price}</button></li>)}
-      </ul>
-    )
+      <div className="app">
+        <h1>Burger Queen</h1>
+        <ul className="menu-rest">
+          {this.state.data.map(
+            (item,index) =>
+              <Item data={item} key={index} onRemove={ () => this._remove(index)} />
+            )
+          }
+        </ul>
+
+
+        {/* <ul className="menu-add">
+          {this.state.data.map(
+            (item,index) =>
+              <Item data={item} key={index} onAdd={ () => this._add(index)} />
+            )
+          }
+        </ul> */}
+
+        <div 
+        className="menu-add">
+          <Button
+            onClick={this._add.bind(this)}
+            name="añadir"
+          />
+
+
+
+          <h4>Total: $ {this._getTotal()}</h4>
+        </div>
+      </div>
+    );
   }
 }
+
+export default Breakfast;
+
