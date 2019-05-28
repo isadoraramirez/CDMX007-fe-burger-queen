b36c4cf112adffcc55e087c79a68e072be366ee0
doctoramalvada <mydemoni@hotmail.com>
2019-05-28 09:24:11 -0500

    quitando codigo muerto

--- src/components/ticket.js
+++ src/components/ticket.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Ticket =(props) =>{
     return (
-        <div className={props.ticket_head}>
-        <div className="ticket_icon_area">
-         <div className="menu_icon icons"  id="micon"></div>
-            <div className="gear_icon icons" id="gcon"></div>
-            <div className="signout_icon icons" id="sicon"></div>
+        <div className={props.ticket}>
+        <div className="ticket">
+        
         </div>
         </div>
     )
