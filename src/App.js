9cee050cf185c142fcf736fedde281a78fef6732
doctoramalvada <mydemoni@hotmail.com>
2019-06-01 17:09:48 -0500

    cambie components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
 import React, { Component } from 'react';
-// import {BrowserRouter, Route} from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import './App.css';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-// import Navbar from './components/navbar';
-// import Home from './components/home';
-// import Breakfast from './components/breakfast';
-
-
-
-
-// import Item from "./components/item"
-
-// import Button from "./components/button";
-
 
 class App extends Component {
-    
-    render() {
-        return (
-          
-          <div>
-            <Layout>
-              <BurgerBuilder/>
-              </Layout> 
-          </div>
-
-
-
-
-            // <BrowserRouter>
-            //     <div className="App">
-            //         <Navbar />
-            //         <Route exact path="/" render={() => <Home title="Bienvenido" />} />
-            //         <Route path="/breakfast" render={() => <Breakfast title="Desayuno" />} />
-                   
-            //         <Route path="/complements" render={() => <Item />} />
-                    
-            // <Route path="/complements" render={() => <Button />} />
-                
-            //     </div>
-            // </BrowserRouter>
-        );
-           
+  render () {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
 export default App;
-
-
-ReactDOM.render(
-  <App 
-  //proop = message se accede con this.prop.message
-  />, 
-  document.getElementById("root")
-);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './index.css';
 import App from './App.js';
 import * as serviceWorker from './registerServiceWorker';
 
